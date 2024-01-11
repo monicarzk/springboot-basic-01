df941d4a1e83a767e11ef84f304b8c0809a9a28f
monicarzk <monikarizki1933@gmail.com>
2024-01-11 21:42:23 +0700

    Setter Injection

---
+++ dev-spring-boot/02-spring-boot-spring-core/03-setter-injection/src/main/java/com/luv2code/springcoredemo/common/CricketCoach.java
@@ -0,0 +1,14 @@
+package com.luv2code.springcoredemo.common;
+
+import com.luv2code.springcoredemo.common.Coach;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CricketCoach implements Coach {
+
+    @Override
+    public String getDailyWorkout() {
+        return "practice fast bowling for 15 minutes";
+    }
+
+}
