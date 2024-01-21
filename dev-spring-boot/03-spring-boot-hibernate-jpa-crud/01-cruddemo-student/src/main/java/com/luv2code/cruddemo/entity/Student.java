a8c66c69b5e454826b80c87bf7b9109bebc771e5
monicarzk <monikarizki1933@gmail.com>
2024-01-21 10:11:11 +0700

    Querying Objects with JPA

--- dev-spring-boot/03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/luv2code/cruddemo/entity/Student.java
+++ dev-spring-boot/03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/luv2code/cruddemo/entity/Student.java
@@ -16,7 +16,7 @@ public class Student {
     private String firstName;
 
     @Column(name="last_name")
-    private String lasttName;
+    private String lastName;
 
     @Column(name="email")
     private String email;
@@ -26,9 +26,9 @@ public class Student {
 
     }
 
-    public Student(String firstName, String lasttName, String email) {
+    public Student(String firstName, String lastName, String email) {
         this.firstName = firstName;
-        this.lasttName = lasttName;
+        this.lastName = lastName;
         this.email = email;
     }
 
@@ -51,11 +51,11 @@ public class Student {
     }
 
     public String getLasttName() {
-        return lasttName;
+        return lastName;
     }
 
     public void setLasttName(String lasttName) {
-        this.lasttName = lasttName;
+        this.lastName = lasttName;
     }
 
     public String getEmail() {
@@ -75,7 +75,7 @@ public class Student {
         return "Student{" +
                 "id=" + id +
                 ", firstName='" + firstName + '\'' +
-                ", lasttName='" + lasttName + '\'' +
+                ", lasttName='" + lastName + '\'' +
                 ", email='" + email + '\'' +
                 '}';
     }
