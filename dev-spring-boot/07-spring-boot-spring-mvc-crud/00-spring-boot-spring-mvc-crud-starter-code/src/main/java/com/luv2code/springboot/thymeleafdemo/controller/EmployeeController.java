6181754daaa4c22b8ac30bebead90e5412eef726
monicarzk <monikarizki1933@gmail.com>
2024-02-08 16:17:35 +0700

    CRUD Database Project - Delete Employee

--- dev-spring-boot/07-spring-boot-spring-mvc-crud/00-spring-boot-spring-mvc-crud-starter-code/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
+++ dev-spring-boot/07-spring-boot-spring-mvc-crud/00-spring-boot-spring-mvc-crud-starter-code/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -67,4 +67,14 @@ public class EmployeeController {
         // use a redirect to prevent duplicate submissions
         return "redirect:/employees/list";
     }
+
+    @GetMapping("/delete")
+    public String delete(@RequestParam("employeeId") int theId) {
+
+        // delete the employee
+        employeeService.deleteById(theId);
+
+        // redirect to the /employees/list
+        return "redirect:/employees/list";
+    }
 }
